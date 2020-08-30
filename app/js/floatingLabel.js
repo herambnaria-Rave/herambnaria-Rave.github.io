79d2e3396125a5cb19d03fc5e33384ed56f07705
herambnariajoshi <59694248+herambnariajoshi@users.noreply.github.com>
2020-08-30 08:57:06 +0530

    Create top search banner and add floating label js

---
+++ app/js/floatingLabel.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var FloatLabel = (function () {
+  // add active class
+  var handleFocus = function handleFocus(e) {
+    var target = e.target;
+    target.parentNode.classList.add('active');
+  }; // remove active class
+
+  var handleBlur = function handleBlur(e) {
+    var target = e.target;
+
+    if (!target.value) {
+      target.parentNode.classList.remove('active');
+    }
+  }; // register events
+
+  var bindEvents = function bindEvents(element) {
+    var floatField = element.querySelector('input');
+    floatField.addEventListener('focus', handleFocus);
+    floatField.addEventListener('blur', handleBlur);
+  }; // get DOM elements
+
+  var init = function init() {
+    var floatContainers = document.querySelectorAll('.floating-label-container');
+    floatContainers.forEach(function (element) {
+      if (element.querySelector('input').value) {
+        element.classList.add('active');
+      }
+
+      bindEvents(element);
+    });
+  };
+
+  return {
+    init: init,
+  };
+})();
+
+FloatLabel.init();
